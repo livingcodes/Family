aadc9cfda9961d21a4f5949af30d2c097f291a70
livingcodes <case.joyce@gmail.com>
2023-01-16 13:30:45 -0600

    Member page: List relationships

--- Family/Pages/Member.cshtml.cs
+++ Family/Pages/Member.cshtml.cs
@@ -4,14 +4,29 @@ public class MemberModel : BasePage
 {
    [BindProperty]
    public Member Member { get; set; } = new();
+   public List<Relationship> Relationships = new();
 
    public void OnGet() {
       var id = GetQueryString("id").ToInt();
       if (id <= 0)
          return;
       Member = db.SelectById<Member>(id);
-      if (Member == null)
+      if (Member == null) {
          Member = new Member();
+         return;
+      }
+      Relationships = db.Select<Relationship>(@"
+         SELECT (m1.FirstName + ' ' + m1.LastName) Name1,
+            (m2.FirstName + ' ' + m2.LastName) Name2,
+            r.[Type]
+         FROM [Relationship] r
+         JOIN Member m1 ON m1.Id = r.MemberId
+         JOIN Member m2 ON m2.Id = r.RelationshipId
+         WHERE m1.Id = @Id OR m2.Id = @Id
+         ", id);
+   }
+   public class Relationship {
+      public string Name1, Name2, Type;
    }
 
    public void OnPost() {
